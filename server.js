aedad1282ea5eae3714a24fc90f143a2e988cfa9
Hemanth-konduri <kondurihemanth62@gmail.com>
2026-06-01 18:40:57 +0530

    Fix static files path for Render deployment

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const path = require('path');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGO_URI);
 
